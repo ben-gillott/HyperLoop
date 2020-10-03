1b7c0b79fd4bb89aa762bcb06a302292a090d869
Benjamin Gillott <bg357@cornell.edu>
2020-10-03 17:13:30 -0400

    dont forget progress

---
+++ Assets/Scripts/CarCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CarCamera : MonoBehaviour
+{
+    [Header("Objects")]
+    [Tooltip("Target to snap to")]
+    public Transform target; //Player
+
+    [Header("Variables")]
+    [Tooltip("Higher value -> faster lock on")]
+    public float smoothSpeed = 0.125f; 
+    public Vector3 offset;
+
+    void LateUpdate() {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        //Look at where the player currently is
+        transform.LookAt(target);
+    }
+
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    [Header("Objects")]
+    [Tooltip("Player object")]
+    public Transform car; //Player'
+
+    [Header("Variables")]
+    [Tooltip("Current speed of the car")]
+    public float speed = 1f; 
+    [Tooltip("Width of each lane")]
+    public float laneWidth = 3;
+    private int lane = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 carDir = new Vector3(0f, 0f, 1f);
+
+        car.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        //Move left
+        if ((Input.GetKeyDown("left") || Input.GetKeyDown("a")) & lane > -1)
+        {
+            car.Translate(Vector3.left * laneWidth);
+            lane -= 1;
+        }
+        else if((Input.GetKeyDown("right") || Input.GetKeyDown("a")) & lane < 1) 
+        {        
+            car.Translate(Vector3.right * laneWidth);
+            lane += 1;
+        }
+    }
+}
