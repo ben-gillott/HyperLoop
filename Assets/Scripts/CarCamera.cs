e51bf59d0247d42f4fa48aafa1aef359f7239718
Benjamin Gillott <bg357@cornell.edu>
2020-10-03 19:26:17 -0400

    circle brrt

--- Assets/Scripts/CarCamera.cs
+++ Assets/Scripts/CarCamera.cs
@@ -8,16 +8,25 @@ public class CarCamera : MonoBehaviour
 
     [Header("Variables")]
     [Tooltip("Higher value -> faster lock on")]
-    public float smoothSpeed = 0.125f; 
-    public Vector3 offset;
+    public float smoothSpeed = 1; 
+    public Vector3 offsetPos;
+    public Quaternion offsetRot = Quaternion.identity;
 
     void LateUpdate() {
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        // Vector3 desiredPosition = target.position + offsetPos;
+        // Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        // transform.position = smoothedPosition;
+
+        // Quaternion desiredRotation = target.rotation;// + offsetRot;
+        // Quaternion smoothedRotation = Quaternion.Lerp(transform.rotation, desiredRotation, smoothSpeed);
+        // transform.rotation = desiredRotation;
+
+        // transform.position = target.position - (new Vector3(0f,0f,10f));
+        // transform.rotation = target.rotation;
+
 
         //Look at where the player currently is
-        transform.LookAt(target);
+        // transform.LookAt(target);
     }
 
 }
--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -11,7 +11,8 @@ public class MovementController : MonoBehaviour
 
     [Header("Variables")]
     [Tooltip("Current speed of the car")]
-    public float speed = 1f; 
+    public float forwardSpeed = 1f; 
+    public float rotationSpeed = 1f; 
     [Tooltip("Width of each lane")]
     public float laneWidth = 3;
     private int lane = 0;
@@ -26,20 +27,25 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Vector3 carDir = new Vector3(0f, 0f, 1f);
-
-        car.Translate(Vector3.forward * Time.deltaTime * speed);
-
         //Move left
         if ((Input.GetKeyDown("left") || Input.GetKeyDown("a")) & lane > -1)
         {
             car.Translate(Vector3.left * laneWidth);
             lane -= 1;
         }
-        else if((Input.GetKeyDown("right") || Input.GetKeyDown("a")) & lane < 1) 
+        else if((Input.GetKeyDown("right") || Input.GetKeyDown("d")) & lane < 1) 
         {        
             car.Translate(Vector3.right * laneWidth);
             lane += 1;
         }
+
+        car.Rotate(-rotationSpeed*Time.deltaTime, 0.0f, 0.0f, Space.Self);
+        car.Translate(Vector3.forward * Time.deltaTime * forwardSpeed);
+
+        // //Input version
+        // else if((Input.GetButton("Fire1")))
+        // {        
+        //     car.Rotate(-10.0, 0.0f, 0.0f, Space.Self);
+        // }
     }
 }
