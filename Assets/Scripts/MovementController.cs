24a5009f9ff6a7d49df88ceb69727e87f9cbda3e
Benjamin Gillott <bg357@cornell.edu>
2020-10-07 22:26:48 -0400

    movement lerp

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -18,13 +18,15 @@ public class MovementController : MonoBehaviour
     [Header("Variables")]
     public float speed;
     public float speedIncrease;
-    
+    public float xLerpSpeed;
     
     //Privates
     private int lane = 0; //Current lane
     private static float fPI = (float)Math.PI;
     private float t = 1.5f*fPI; //Position in the loop in spherical, from 0 to 2pi
     private float lastRotX = 0;
+    private float targetX = 0;
+    private float x = 0;
 
     // Update is called once per frame
     void Update()
@@ -39,39 +41,23 @@ public class MovementController : MonoBehaviour
             lane += 1;
         }
 
-        // car.Rotate(-speed*Time.deltaTime, 0.0f, 0.0f, Space.Self);
-        // car.Translate(Vector3.forward * Time.deltaTime * speed);
 
         //Position : spherical coords
-        float x = trackWidth*lane;
+        targetX = trackWidth*lane; //Set target
+        x = Mathf.Lerp(x, targetX, Time.deltaTime * xLerpSpeed);//Lerp x towards target
+
         float z = radius*(float)Math.Cos(t) + centerZ;
         float y = radius*(float)Math.Sin(t) + centerY;
         gameObject.transform.position = new Vector3(x,y,z);
         
 
         //Direction : tangent to circle
-
-        // float vz = gameObject.transform.position.z - centerZ;
-        // float vy = gameObject.transform.position.y - centerY;
-        
-        // Vector3 orthDirection = new Vector3(0f, -vz, vy);
-        // Vector3 orthDirection = new Vector3(0f, (float)Math.Cos(t), (float)Math.Sin(t));
-        // gameObject.transform.forward = orthDirection;
-
         float rotX = (360f*(-1)*t/(2f*fPI)-90f)%360f;
-        // Debug.Log(xRot);
-        // Vector3 eulers = gameObject.transform.rotation.eulerAngles;
-        // gameObject.transform.rotation = Quaternion.Euler(new Vector3(xRot,eulers.y,eulers.z));
         Quaternion localRotation = Quaternion.Euler(rotX - lastRotX, 0f, 0f);
         transform.rotation = transform.rotation * localRotation;
         lastRotX = rotX;
 
-        // Vector3 rot = gameObject.transform.eulerAngles;
-        // gameObject.transform.eulerAngles = new Vector3(xRot, rot.y, rot.z);
-
-        //update t
-
-
+        //Update T
         t += speed * Time.deltaTime * fPI;
     }
 
